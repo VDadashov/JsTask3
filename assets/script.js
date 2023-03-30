334ec66d2352a97632bc189e5ee4356af44e57b4
Vusal <vusald79@gmail.com>
2023-03-30 06:15:51 +0400

    update-task

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,38 @@
 let inp = document.getElementById('inp');
-let btn = document.getElementById('btn');
+let addBtn = document.getElementById('btn-add');
+let removeBtn = document.getElementById('btn-remove')
 let list = document.getElementById('list');
 let todo = [];
 
-function add(){
+function additems(){
     if ((inp.value != '') && (!todo.includes(`${inp.value}`))) {
         todo.push(inp.value);
     }
 
     let data = '';
     for(let item of todo){
+        list.style =
         data += `<li class="list-group-item"> ${item} </li>` 
-    }
+    }   
     inp.value = '';
     list.innerHTML = data;
 }
 
-btn.addEventListener('click', add);
+function removeItems(){
+    if (todo.length > 0) {
+        todo.pop(todo[0]);
+      }
+    
+    if (list.children.length > 0) {
+        list.removeChild(list.lastChild);
+      }
+    
+}
+
+addBtn.addEventListener('click', additems);
+removeBtn.addEventListener('click', removeItems);
+inp.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      additems();
+    }
+  });
